83c2498f195e5effe9de0505dd7a7c6a595660f0
Paulo dos Santos <paulo_santos20@outlook.com.br>
2023-10-06 12:05:20 -0300

    fix: pt-BR currency

--- lib/format.ts
+++ lib/format.ts
@@ -1,5 +1,5 @@
 export const formatPrice = (price: number) => {
-  return new Intl.NumberFormat("en-", {
+  return new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL"
   }).format(price)
